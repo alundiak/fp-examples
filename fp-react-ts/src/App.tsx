fd9ecad462c653a0f79fb1bb90fbe8b587e31636
Andrii Lundiak <landike@gmail.com>
2023-12-05 22:11:55 +0100

    Extended React FP with pure vs impure components, memoized vs callback-ed behavior.

--- fp-react-ts/src/App.tsx
+++ fp-react-ts/src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 
 import {
   FunctionalComponent,
-  MyPureComponent,
+  MyPureClassComponent,
+  MyImpureFunctionalComponent,
+  NoStateReactPureComponent,
   StateComponent,
   StateAndPropsComponent,
   MySideEffect,
+  CounterStateReactComponent,
   HocFunc,
   CreatedWithHocComponent,
   MyMemoComponentWithFuncComp,
@@ -14,7 +17,9 @@ import {
   MyMemoComponentWithFatArrow,
   ThePowerOfHooks,
   Name,
-  ReversedName
+  ReversedName,
+  CounterFunctionalComponentWithHooks,
+  MemoVsCallback,
   // WithAllMyHeart
 } from './code-react';
 
@@ -27,14 +32,18 @@ import {
 const FPExamples = () => (
   <div className="container">
     <FunctionalComponent msg="Functional Component" />
-    <MyPureComponent msg="Pure Component" />
+    <NoStateReactPureComponent msg="no state React.PureComponent" />
+    <MyPureClassComponent name="Andrii" lastName="Lundiak" />
+    <MyImpureFunctionalComponent name="Johannes" lastName="Lundiak" />
     <StateComponent msg="Component with state" />
     <StateAndPropsComponent msg="Component with state and props" a={3} b={3} />
+    <CounterStateReactComponent />
     <MySideEffect />
+
     <HocFunc
       msg1="High Order Functional Component wrapping Pure Component"
       msg2="Pure Component (wrapped by HOC)"
-      anotherComponent={MyPureComponent}
+      anotherComponent={NoStateReactPureComponent}
     />
     <CreatedWithHocComponent />
     <MyMemoComponentWithFuncComp msg="HOC - memo() #1" />
@@ -44,8 +53,14 @@ const FPExamples = () => (
     <MyMemoComponentWithUnNamedFunc msg="HOC - memo() #3" />
     {/* @ts-ignore */}
     <MyMemoComponentWithFatArrow msg="HOC - memo() #4" />
+
+    <CounterFunctionalComponentWithHooks />
+
     <ThePowerOfHooks />
     <ThePowerOfHooks msg="Use Hooks and be happy." />
+
+    <MemoVsCallback msg="useMemo() vs useCallback()" />
+
     <Name>Hello</Name>
     <ReversedName>Hello</ReversedName>
     {/* <WithAllMyHeart /> */}
--- fp-react-ts/src/code-react.jsx
+++ fp-react-ts/src/code-react.jsx
@@ -1,6 +1,5 @@
 /* eslint react/no-multi-comp: 0 */
-
-import React from 'react';
+import React, { useState, useCallback, useMemo } from 'react'
 // import PropTypes from 'prop-types';
 
 export function FunctionalComponent(props) {
@@ -11,7 +10,35 @@ export function FunctionalComponent(props) {
 // TODO
 // FunctionalComponent.propTypes
 
-export class MyPureComponent extends React.PureComponent {
+//
+// Another examples of Pure and Impure Components with React
+// https://www.freecodecamp.org/news/memoization-in-javascript-and-react/#react-memoization-example
+//
+
+// Pure Class component
+export class MyPureClassComponent extends React.Component {
+  render() {
+    return (
+      <div>My "pure" name is {this.props.name} {this.props.lastName}</div>
+    )
+  }
+}
+
+// Impure Functional component
+export function MyImpureFunctionalComponent({ name, lastName }) {
+  return (
+    <div>My "impure" name is {name + Math.random()} {lastName + Math.random()}</div>
+  )
+}
+
+//
+// https://www.freecodecamp.org/news/memoization-in-javascript-and-react/#react-memoization-example
+// Class components that extend the React.PureComponent class have some performance improvements and render optimizations. 
+// This is because React implements the shouldComponentUpdate() method for them with a shallow comparison for props and state.
+// When child components rendered EVEN if same props received we need to implement 
+// memoization and optimize this situation, we need to extend the React.PureComponent class in our child component
+//
+export class NoStateReactPureComponent extends React.PureComponent {
   // aka stateless pure component
   /* eslint no-useless-constructor: 0 */
   constructor(props) {
@@ -47,7 +74,7 @@ export class StateComponent extends React.Component {
           type="button"
           className="btn btn-sm btn-link"
           onClick={this.onClick}>Click Me
-                </button>
+        </button>
       </div>
     );
   }
@@ -79,14 +106,62 @@ export class StateAndPropsComponent extends React.Component {
           type="button"
           className="btn btn-sm btn-link"
           onClick={this.onClick}>Click Me
-                </button>
+        </button>
       </div>
     );
   }
 }
 
 //
-// Avoid Side Effect
+// https://www.freecodecamp.org/news/memoization-in-javascript-and-react/#purecomponent-class
+//
+// class Child extends React.Component { // IMPURE (with redundancy memory usage)
+class Child extends React.PureComponent { // PURE (PureComponent gives us some memoization)
+  render() {
+    // When wrapped by <React.StrictMode> it logs TWICE (no matter if extended from React.Component or React.PureComponent)
+    console.log("Skinny Jack");
+    // But what is IMPORTANT HERE that when extends from React.PureComponent <Child /> renders ONLY ONCE - "the most pure way" as FP expects.
+
+    return (
+      <p>{this.props.name}</p>
+    )
+  }
+}
+
+export class CounterStateReactComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0 }
+  }
+
+  handleIncrement = () => {
+    this.setState(prevState => {
+      return { count: prevState.count - 1 };
+    })
+  }
+
+  handleDecrement = () => {
+    this.setState(prevState => {
+      return { count: prevState.count + 1 };
+    })
+  }
+
+  render() {
+    console.log("Parent render"); // When wrapped by <React.StrictMode> it logs TWICE
+
+    return (
+      <div>
+        <p>Counter (React.Component) + Child (React.PureComponent)</p>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+        <h3>{this.state.count}</h3>
+        <Child name={"Skinny Jack"} />
+      </div>
+    )
+  }
+}
+//
+// FP says "Avoid Side Effect" and here is example how to NOT do???
 //
 
 export class MySideEffect extends React.Component {
@@ -117,11 +192,10 @@ export class MySideEffect extends React.Component {
           type="button"
           className="btn btn-sm btn-link"
           onClick={() => this.toggleFunc(flagState)}>Click me
-                </button>
+        </button>
       </div>
     );
   }
-
 }
 
 //
@@ -175,8 +249,36 @@ export const CreatedWithHocComponent = createHOC(StateAndPropsComponent, {
 });
 // typeof HocComponent => "function"
 
+// https://www.freecodecamp.org/news/memoization-in-javascript-and-react/#purecomponent-class
+// In functional components we can't extend the React.PureComponent class. 
+// Instead, React offers one HOC and two hooks to deal with memoization.
+
+// FP: "power" because `props.msg` literally left alone which helps to avoid mutation.
+export const ThePowerOfHooks = (props) => {
+  const [myMessage, setMyMessage] = useState(props.msg || 'Functional Component using hooks'); //eslint-disable-line
+
+  // TODO react.useEffect()
+
+  const onClick = () => {
+    setMyMessage('Yeah, you clicked again. But this time, inside of Functional Component with state using Hooks.');
+  };
+
+  return (
+    <div>
+      <span>
+        {myMessage}
+      </span>
+      <button
+        type="button"
+        className="btn btn-sm btn-link"
+        onClick={onClick}>Click Me
+      </button>
+    </div>
+  );
+};
+
 //
-// HOC + Memoization
+// HOC + Memoization (memo does a similar job to PureComponent, avoiding unnecessary re-renders of the components it wraps)
 //
 export const MyMemoComponentWithFuncComp = React.memo(FunctionalComponent);
 
@@ -201,29 +303,73 @@ export const MyMemoComponentWithFatArrow = React.memo((props) => {
   return <div>FunctionalComponent4 (via fat arrow func.) inside of {msg}.</div>;
 });
 
-export const ThePowerOfHooks = (props) => {
-  const [myMessage, setMyMessage] = React.useState(props.msg || 'Functional Component using hooks'); //eslint-disable-line
+//
+// React.memo() + React.useCallback() 
+//
 
-  // TODO react.useEffect()
+// React.memo() doesn't work if the prop being passed to the component is a function
+// So passing prop `name` as function is bad.
+// In reality a new function is being created on EVERY parent component re-render. 
+// So if a new function is being created, that means we have a NEW PROP and that means our child component should re-render as well.
+export const ExoticMemoizedChildComponent = React.memo(function Child({ name }) {
+  console.log("Skinny Jack (2)", name);
+  return (
+    <>
+      {name()}
+      <div>Really Skinny Jack (2)</div>
+    </>
+  )
+});
 
-  const onClick = () => {
-    setMyMessage('Yeah, you clicked again. But this time, inside of Functional Component with state using Hooks.');
-  };
+export function CounterFunctionalComponentWithHooks() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => setCount(count + 1);
+  const handleDecrement = () => setCount(count - 1);
 
   return (
     <div>
-      <span>
-        {myMessage}
-      </span>
-      <button
-        type="button"
-        className="btn btn-sm btn-link"
-        onClick={onClick}>Click Me
-            </button>
-    </div>
-  );
-};
+      {console.log('parent')}
+      <p>CounterWithHooks FC + ExoticMemoizedChildComponent (React.memo)</p>
+      <button onClick={() => handleIncrement()}>Increment</button>
+      <button onClick={() => handleDecrement()}>Decrement</button>
 
+      <h2>{count}</h2>
+
+      {/* Bad, not memoized, re-renders ALWAYS */}
+      {/* <ExoticMemoizedChildComponent name={() => console.log('Skinny Jack (basic function)')} /> */}
+      {/* Better. Re-renders ONLY ONCE */}
+      <ExoticMemoizedChildComponent name={useCallback(() => { console.log('Skinny Jack (w useCallback())') }, [])} />
+    </div>
+  )
+}
+
+//
+// React.useMemo() hook
+//
+// https://www.freecodecamp.org/news/memoization-in-javascript-and-react/#when-to-use-the-usememo-hook
+// useMemo is a hook very similar to useCallback, but instead caching a function, useMemo will cache the return value of a function.
+// You can use useMemo in a very similar way to the memo HOC. The difference is that useMemo is a hook with an array of dependencies, 
+// and memo is a HOC that accepts as parameter an optional function that uses props to conditionally update the component.
+
+export const MemoVsCallback = ({ msg }) => {
+
+  var num = 1
+  const answerMemoized = useMemo(() => num + 1, [num])
+  console.log(answerMemoized); // 2
+
+  var num = 1
+  const AnswerCallbacked = useCallback(() => num + 1, [num]);
+  console.log(AnswerCallbacked); // () => num + 1
+
+  return (
+    <>
+      <h3>{msg}</h3>
+      answerMemoized: <div>{answerMemoized}</div>
+      answerCallbacked: <div><AnswerCallbacked /></div>
+    </>
+  );
+}
 //
 // CURRYING (may also interfere with term Chaining)
 // ReactJS HOC takes another COMPONENT as argument.
@@ -243,7 +389,6 @@ const reverseShort = PassedComponent => ({ children, ...props }) => (
   </PassedComponent>
 );
 
-
 const reverse = (PassedComponent) => {
   return function ReversedName({ children, ...props }) {
     return (
@@ -253,7 +398,6 @@ const reverse = (PassedComponent) => {
     );
   }
 };
-
 
 export const Name = (props) => <div>{props.children}</div>;
 export const ReversedName = reverse(Name);
